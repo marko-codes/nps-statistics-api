874e52af3fb343d24de789a23a4c012f45b41377
Marko Maricic <maricicmarko96@gmail.com>
2023-07-04 16:10:04 +0200

    Update WebMvcConfig.java

--- src/main/java/dev/maricicmarko/npsstatisticsapi/configuration/WebMvcConfig.java
+++ src/main/java/dev/maricicmarko/npsstatisticsapi/configuration/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://nps-markomaricic.up.railway.app")
                 .allowedMethods("GET");
 
     }
